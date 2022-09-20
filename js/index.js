adff7ab3b23440a357351f49d9c02136d5f9c9df
evenh <2364451784@qq.com>
2022-09-20 17:14:51 +0800

    update

--- js/index.js
+++ js/index.js
@@ -2,6 +2,8 @@ window.addEventListener('load',function() {
     let criticalDistance = window.innerHeight - 200
     let min_box = document.querySelectorAll('.min_box')
     let banner = document.querySelector('.right_sideBar')
+    let flexTop = banner.offsetTop
+    console.log(flexTop);
     //滑动卡片效果
     window.addEventListener('resize',function () {
         criticalDistance = window.innerHeight - 200
@@ -9,6 +11,7 @@ window.addEventListener('load',function() {
     })
     window.addEventListener('scroll', function () {
         dynamicShow()
+        User_flex()
     })
     //滑动卡片的封装函数
     function dynamicShow() {
@@ -22,6 +25,17 @@ window.addEventListener('load',function() {
         }
       }
       
-    
+    //用户栏跟随滚轮固定的封装函数
+    function User_flex() {
+      // let flexTop = banner.offsetTop
+      if(window.pageYOffset >= flexTop) {
+        // 修改定位为fixed定位
+        banner.style.position = 'fixed'
+        banner.style.top = 0 + 'px'
+      } else {
+        banner.style.position = 'absolute'
+        banner.style.top = flexTop + 'px'
+      }
+    }
 
 })
